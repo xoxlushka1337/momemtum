45e0bbaefa3bf035773a6cf78320e35db8de91d9
Ulyana <ulana.xoxlushka@gmail.com>
2023-02-06 14:06:46 +0300

    feat: add clock and calendar

---
+++ stage1-tasks-momentum/js/script.js
@@ -0,0 +1,28 @@
+function showTime() {
+  let date = new Date();
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  if (hours < 10) hours = '0' + hours;
+  if (minutes < 10) minutes = '0' + minutes;
+  if (seconds < 10) seconds = '0' + seconds;
+  let time = `${hours}:${minutes}:${seconds}`;
+  document.querySelector('.time').innerHTML = time;
+  showDate();
+  setTimeout(showTime, 1000);
+}
+showTime();
+
+function showDate() {
+  const date = new Date();
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'UTC',
+    weekday: 'long',
+  };
+  const currentDate = date.toLocaleDateString('en-US', options);
+  document.querySelector('.date').innerHTML = currentDate;
+}
+showDate();
